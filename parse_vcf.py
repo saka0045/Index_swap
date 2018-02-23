e6f5393941d78f82323b8ed721b95443c8cd48f6
Yuta Sakai <sakai619@gmail.com>
2018-02-23 13:40:01 -0600

    Added feature to pull coverage data to another column

--- parse_vcf.py
+++ parse_vcf.py
@@ -9,18 +9,18 @@ Created on Tue Feb  6 10:29:13 2018
 import gzip
 
 # open files
-vcfFile = gzip.open('/dlmp/sandbox/runs/WESPP/NGS26-WESPP_F137035_NS0001_NS0001_H2YF5DMXX/samples/WESPP-NA12878-CTRL/ggps_results/OUTPUT/PI/exome/WESPP-NA12878-CTRL/variants/variants.vcf.gz.bior_annotate.vcf.gz', 'rb')
-#vcfFile = open('/Users/m006703/Index_Swap/files/practice.vcf', 'r')
+#vcfFile = gzip.open('/dlmp/sandbox/runs/WESPP/NGS26-WESPP_F137035_NS0001_NS0001_H2YF5DMXX/samples/WESPP-NA12878-CTRL/ggps_results/OUTPUT/PI/exome/WESPP-NA12878-CTRL/variants/variants.vcf.gz.bior_annotate.vcf.gz', 'rb')
+vcfFile = open('/Users/m006703/Index_Swap/files/practice.vcf', 'r')
 
 # Go to the header line
 for line in vcfFile:
     if line.startswith('#CHROM'):
         firstline = line.split('\t')
         sampleName = firstline[9].rstrip()
-	break
+        break
         
-outFile = open('/dlmp/sandbox/cgslIS/Yuta/Index_Swap/files/' + sampleName + '_gnomAD.vcf', 'w')
-        
+#outFile = open('/dlmp/sandbox/cgslIS/Yuta/Index_Swap/files/' + sampleName + '_gnomAD.vcf', 'w')
+outFile = open('/Users/m006703/Index_Swap/files/gnomAD.vcf', 'w')       
 # Start collecting data after the header line           
 chrom = []
 pos = []
@@ -31,6 +31,8 @@ qual = []
 qualfilter = []
 info = []
 resultformat = []
+resultmatrix = []
+coveragelist = []
 
 # Collect data that only has value for non-population specific gnomAD frequency
 for line in vcfFile:
@@ -41,7 +43,10 @@ for line in vcfFile:
         for item in splitinfoline:
             if item.startswith('gnomAD_r201_GRCh37.INFO.AF='):
                 gnomADInfo = float(item.split('=')[1])
-                if 0 < gnomADInfo <= 0.001: # Adjust this to change cutoff
+                if 0 < gnomADInfo <= 0.01: # Cutoff for gnomAD variant frequency
+                    formatlist = result[8].split(':')
+                    formatresult = result[9].rstrip().split(':')
+                    coverage = formatresult[formatlist.index('DP')]
                     chrom.append(result[0])
                     pos.append(result[1])
                     snpid.append(result[2])
@@ -51,12 +56,14 @@ for line in vcfFile:
                     qualfilter.append(result[6])
                     info.append(gnomADInfo)
                     resultformat.append(result[8])
+                    resultmatrix.append(result[9].rstrip())
+                    coveragelist.append(coverage)
                    
-outFile.write('CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tgnomADINFO\tFORMAT\n')
+outFile.write('CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tgnomADINFO\tFORMAT\t\tCOVERAGE\n')
 
 for index in range(len(chrom)):
     resultList = []
-    resultList = [chrom[index], pos[index], ref[index], alt[index], qual[index], qualfilter[index], str(info[index]), resultformat[index]]
+    resultList = [chrom[index], pos[index], snpid[index], ref[index], alt[index], qual[index], qualfilter[index], str(info[index]), resultformat[index], resultmatrix[index], coveragelist[index]]
     outFile.write('\t'.join(resultList) + '\n')
 
 vcfFile.close()
