af6e505e459343149eaa434accdadef688616704
Yuta Sakai <sakai619@gmail.com>
2018-03-24 15:39:52 -0500

    Added SNP counter for makeCoverageList.py

--- makeCoverageList.py
+++ makeCoverageList.py
@@ -13,12 +13,14 @@ Assumes that parse_vcf.py fequency cutoff is set at 5%
 
 import os
 
+# Open all of the files created by parse_vcf.py
 basedir = '/Users/m006703/Index_Swap/files/'
 fileList = os.listdir(basedir)
 
 frequencyList = []
 coverageList = []
 
+# Parse out frequency and coverage information from all files
 for file in fileList:
     filePath = basedir + file
     openFile = open(filePath, 'r')
@@ -31,17 +33,40 @@ for file in fileList:
         coverageList.append(coverage)
     openFile.close()
         
-FivePercentCoverageList = open(basedir + '5PercentCoverageList' , 'w')
-OnePercentCoverageList = open(basedir + '1PercentCoverageList', 'w')
-OneTenthPercentCoverageList = open(basedir + '0.1PercentCoverageList', 'w')
+
+# Create result directory inside basedir if it doesn't exist
+if not os.path.exists(basedir + 'result/'):
+    os.makedirs(basedir + 'result/')    
+
+# Creating empty files based on the frequency cutoff    
+FivePercentCoverageList = open(basedir + 'result/5PercentCoverageList' , 'w')
+OnePercentCoverageList = open(basedir + 'result/1PercentCoverageList', 'w')
+OneTenthPercentCoverageList = open(basedir + 'result/0.1PercentCoverageList', 'w')
+SNPCounter = open(basedir + 'result/SNPCounter.txt', 'w')
+
+# Counting SNPS with certain coverage
+FivePercentCounter = 0
+OnePercentCounter = 0
+OneTenthPercentCounter = 0
 
 for index in range(len(frequencyList)):
+    if int(coverageList[index]) >= 100:
+        FivePercentCounter += 1
     FivePercentCoverageList.write(frequencyList[index] + '\t' + coverageList[index] + '\n')
     if float(frequencyList[index]) <= 1.0:
+        if int(coverageList[index]) >= 100:
+            OnePercentCounter += 1
         OnePercentCoverageList.write(frequencyList[index] + '\t' + coverageList[index] + '\n')
         if float(frequencyList[index]) <= 0.1:
+            if int(coverageList[index]) >= 100:
+                OneTenthPercentCounter += 1
             OneTenthPercentCoverageList.write(frequencyList[index] + '\t' + coverageList[index] + '\n')
-            
+
+SNPCounter.write('SNPs with frequency greater than 5% and coverage greater than 100: ' + str(FivePercentCounter) + '\n'\
+                 'SNPs with frequency greater than 1% and coverage greater than 100: ' + str(OnePercentCounter) + '\n'\
+                 'SNPs with frequency greater than 0.1% and cverage greater than 100: ' + str(OneTenthPercentCounter) + '\n')
+
 FivePercentCoverageList.close()
 OnePercentCoverageList.close()
 OneTenthPercentCoverageList.close()
+SNPCounter.close()
